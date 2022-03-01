10ee391b6d3f0dfe13381ee24759d373434935a2
Fernando Pontes <fernandopontes@outlook.com>
2022-03-01 09:42:09 -0300

    ci: :sparkles: config sonarqube for run test software

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test("add 1 + 2 to be equal 3", () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test("add 1 + 3 to be equal 4", () => {
+    expect(sum(1, 3)).toBe(4);
+});
