f5ed95ad4dc61d4192098f8e6601cd9004b6d1f0
Fernando Pontes <fernandopontes@outlook.com>
2022-03-09 19:59:09 -0300

    refactor: :hammer: edit function test sum

--- src/tests/_sum.test.js
+++ src/tests/_sum.test.js
@@ -4,6 +4,6 @@ test("add 1 + 2 to be equal 3", () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test("add 1 + 3 to be equal 4", () => {
-    expect(sum(1, 3)).toBe(4);
+test("add 2 + 3 to be equal 5", () => {
+    expect(sum(2, 3)).toBe(5);
 });
